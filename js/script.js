2559f8286cea69610df76a5309ca8400465cd31e
Pieternellemaris <118263741+Pieternellemaris@users.noreply.github.com>
2025-12-12 13:55:03 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -149,3 +149,13 @@ exhibLink.addEventListener('click', (e) => {
 
 
       if(history.replaceState) history.replaceState({}, "", "/");
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+  console.log("Mobile device detected");
+} else {
+  console.log("Desktop device detected");
+}
