28671cd190e1f47f3dc11c819902b6da70107e8b
Pieternellemaris <118263741+Pieternellemaris@users.noreply.github.com>
2025-12-13 17:17:01 +0000

    Add files via upload

--- js/script_2.js
+++ js/script_2.js
@@ -13,3 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
     fullPage.classList.remove("show");
   });
 });
+function myFunction(x) {
+  x.classList.toggle("change");
+  const btn = document.getElementById("hiddenDrop");
+
+  var btnDisplay = window.getComputedStyle(btn).getPropertyValue("display"); 
+  if (x.classList.contains("change")) {
+console.log('hi');
+    btn.style.display="block"
+  } else {
+        btn.style.display="none"
+    console.log('no')
+  }
+}
